f2588fd854f7d54b49b3931d29ce04a87fe858b4
Joshua Agboola <slimmy0310@gmail.com>
2023-12-15 11:21:17 +0100

    created two functions to deselect and select answers

--- script.js
+++ script.js
@@ -107,3 +107,19 @@ function loadQuiz() {
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
 }
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
+
+function getSelected() {
+  let answer;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
